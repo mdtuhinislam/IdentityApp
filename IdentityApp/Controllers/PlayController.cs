d8944754dc16ed4491b3ee0b9fb55e661d60799c
mdtuhinislam <mdtuhinislam@gmail.com>
2024-08-24 23:52:33 +0600

    play controller added

---
+++ IdentityApp/Controllers/PlayController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace IdentityApp.Controllers
+{
+    [Authorize]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PlayController : ControllerBase
+    {
+        public PlayController()
+        {
+            
+        }
+
+        [HttpGet("get-players")]
+        public IActionResult GetPlayers()
+        {
+            return new JsonResult(new { Message = "I am from player controller" });
+        }
+    }
+}
